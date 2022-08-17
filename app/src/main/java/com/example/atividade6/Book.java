0a93bab7d8cf425197e1f76f0cd33a0ea7e7f0b0
sequeto <joaopedrosequeto@gmail.com>
2022-08-17 01:08:03 -0300

    Adicionando Notações da Tabela Livro e Criando Data Access Object

--- app/src/main/java/com/example/atividade6/Book.java
+++ app/src/main/java/com/example/atividade6/Book.java
@@ -1,6 +1,15 @@
 package com.example.atividade6;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Book")
 public class Book {
+    @PrimaryKey(autoGenerate = true)
+    private Long id;
+
+    @ColumnInfo(name = "title")
     private String title;
 
     public Book(String title){
---
+++ app/src/main/java/com/example/atividade6/BookDao.java
@@ -0,0 +1,7 @@
+package com.example.atividade6;
+
+import androidx.room.Dao;
+
+@Dao
+public interface BookDao {
+}
